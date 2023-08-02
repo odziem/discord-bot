59271ad5e6570c8d2438c54f9414c7a2bb6f1161
Adam Odziemkowski <odziem@users.noreply.github.com>
2023-08-02 06:11:03 -0400

    Register commands with Discord

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -1,7 +1,31 @@
-function clientReadyHandler(client) {
+const {
+  REST,
+  Routes,
+} = require('discord.js');
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+async function clientReadyHandler(client) {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  try {
+    console.log(`Started refreshing ${client.commands.size} commands!`);
+
+    const data = await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      {
+        body: client.commands.map((command) => {
+          return command.data.toJSON();
+        })
+      }
+    );
+
+    console.log(`Successfully reloaded ${data.length} commands!`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
   clientReadyHandler,
-};
+};;
--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ require('dotenv').config();
 
 const {
   Client,
+  Collection,
   Events,
   GatewayIntentBits,
-  REST,
 } = require('discord.js');
 
 const { clientReadyHandler } = require('./events/clientReady');
+
+const pingCommand = require('./commands/ping');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
   ]
 });
+
+client.commands = new Collection();
+
+client.commands.set(pingCommand.data.name, pingCommand);
 
 client.on(Events.ClientReady, clientReadyHandler);
 
