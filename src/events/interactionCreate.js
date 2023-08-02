0a796eddcdfa4b76316d19738de89a14fd2f8cc8
Adam Odziemkowski <odziem@users.noreply.github.com>
2023-08-02 08:22:53 -0400

    Improved interaction create handler.

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,7 +1,32 @@
 async function interactionCreateHandler(interaction) {
+  if (!interaction.isChatInputCommand()) {
+    return;
+  }
+
   const command = interaction.client.commands.get(interaction.commandName);
 
-  await command.execute(interaction);
+  if (!command) {
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+
+    console.log(`${interaction.user.username} used command ${interaction.commandName}`);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error executing this command!',
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: 'There was an error executing this command!',
+        ephemeral: true,
+      });
+    }
+  }
 }
 
 module.exports = {
