f16bb7770210f6d0aa163528359b9a428696eb5c
Adam Odziemkowski <odziem@users.noreply.github.com>
2023-08-04 06:08:42 -0400

    Implement astro command

---
+++ src/commands/astro.js
@@ -0,0 +1,52 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+} = require('discord.js');
+
+const { fetchForecast } = require('../requests/forecast');
+
+const data = new SlashCommandBuilder()
+  .setName('astro')
+  .setDescription('Replies with the astronomical information for the day!')
+  .addStringOption((option) => {
+    return option
+      .setName('location')
+      .setDescription('The location can be a city, zip/postal code, or a latitude and longitude.')
+      .setRequired(true);
+  });
+
+async function execute(interaction) {
+  await interaction.deferReply();
+
+  const location = interaction.options.getString('location');
+
+  try {
+    const { weatherData, locationName } = await fetchForecast(location);
+
+    const embed = new EmbedBuilder()
+      .setColor(0x3f704d)
+      .setTitle(`Astronomical forecast for ${locationName}...`)
+      .setTimestamp()
+      .setFooter({
+        text: 'Powered by the weatherapi.com API',
+      });
+
+    for (const day of weatherData) {
+      embed.addFields({
+        name: day.date,
+        value: `🌅 Sunrise: ${day.sunriseTime}\n🌇 Sunset: ${day.sunsetTime}\n🌔 Moonrise: ${day.moonriseTime}\n🌘 Moonset: ${day.moonsetTime}`,
+      });
+    }
+
+    await interaction.editReply({
+      embeds: [embed],
+    });
+  } catch (error) {
+    await interaction.editReply(error);
+  }
+}
+
+module.exports = {
+  data,
+  execute,
+};
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const { clientReadyHandler } = require('./events/clientReady');
 const { interactionCreateHandler } = require('./events/interactionCreate');
 
 const pingCommand = require('./commands/ping');
+const astroCommand = require('./commands/astro');
 const forecastCommand = require('./commands/forecast');
 
 const client = new Client({
@@ -22,6 +23,7 @@ const client = new Client({
 client.commands = new Collection();
 
 client.commands.set(pingCommand.data.name, pingCommand);
+client.commands.set(astroCommand.data.name, astroCommand);
 client.commands.set(forecastCommand.data.name, forecastCommand);
 
 client.once(Events.ClientReady, clientReadyHandler);
